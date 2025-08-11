d1b7bccc5533f3a00844855810f7a42fc0546bdd
VaibhavSkandh <vaibhavskandh@gmail.com>
2025-08-11 13:57:48 +0530

    feature:task details UI

--- src/Maincontent/Routed_files/TaskDetails.tsx
+++ src/Maincontent/Routed_files/TaskDetails.tsx
@@ -6,55 +6,55 @@ interface TaskDetailsProps {
   taskTitle: string;
 }
 
-interface ImportantPageProps {
-  onTaskSelect: (task: { id: string; title: string }) => void;
-}
-
 const TaskDetails: React.FC<TaskDetailsProps> = ({ onClose, taskTitle }) => {
   return (
     <aside className={styles.taskDetailsPanel}>
-      <div className={styles.header}>
-        <div className={styles.titleSection}>
-          <span className={styles.checkbox}></span>
-          <h2 className={styles.taskTitle}>{taskTitle}</h2>
-          <span className={`${styles.starIcon} material-icons`}>star_border</span>
+      <button onClick={onClose} className={styles.closeButton}>
+        <span className="material-icons">close</span>
+      </button>
+      <div className={styles.taskHeaderSection}>
+        <div className={styles.header}>
+          <div className={styles.titleSection}>
+            <span className={styles.checkbox}></span>
+            <h2 className={styles.taskTitle}>{taskTitle}</h2>
+            <span className={`${styles.starIcon} material-icons`}>star_border</span>
+          </div>
         </div>
-        <button onClick={onClose} className={styles.closeButton}>
-          <span className="material-icons">close</span>
-        </button>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.section}>
+        <div className={styles.addStepSection}>
           <span className={`${styles.addStepIcon} material-icons`}>add</span>
           <span className={styles.addStepText}>Add step</span>
         </div>
-        <div className={styles.section}>
-          <span className={`${styles.icon} material-icons`}>wb_sunny</span>
-          <span className={styles.sectionText}>Added to My Day</span>
-          <span className={`${styles.closeIcon} material-icons`}>close</span>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.myDaySection}>
+          <span className={`${styles.myDayIcon} material-icons`}>wb_sunny</span>
+          <span className={styles.myDayText}>Added to My Day</span>
         </div>
-        <div className={styles.section}>
-          <span className={`${styles.icon} material-icons`}>schedule</span>
-          <span className={styles.sectionText}>Remind me</span>
+        <div className={styles.actionSection}>
+          <div className={styles.actionItem}>
+            <span className={`${styles.actionIcon} material-icons`}>schedule</span>
+            <span className={styles.actionText}>Remind me</span>
+          </div>
+          <div className={styles.actionItem}>
+            <span className={`${styles.actionIcon} material-icons`}>calendar_today</span>
+            <span className={styles.actionText}>Add due date</span>
+          </div>
+          <div className={styles.actionItem}>
+            <span className={`${styles.actionIcon} material-icons`}>repeat</span>
+            <span className={styles.actionText}>Repeat</span>
+          </div>
         </div>
-        <div className={styles.section}>
-          <span className={`${styles.icon} material-icons`}>calendar_today</span>
-          <span className={styles.sectionText}>Add due date</span>
-        </div>
-        <div className={styles.section}>
-          <span className={`${styles.icon} material-icons`}>repeat</span>
-          <span className={styles.sectionText}>Repeat</span>
-        </div>
-        <div className={styles.section}>
-          <span className={`${styles.icon} material-icons`}>attach_file</span>
-          <span className={styles.sectionText}>Add file</span>
+        <div className={styles.addFileSection}>
+          <span className={`${styles.addFileIcon} material-icons`}>attach_file</span>
+          <span className={styles.addFileText}>Add file</span>
         </div>
         <div className={styles.noteSection}>
           <textarea className={styles.noteInput} placeholder="Add note"></textarea>
         </div>
       </div>
       <div className={styles.footer}>
-        <div className={styles.creationTime}>Created a few moments ago</div>
+        <div className={styles.creationTime}>Created 21 minutes ago</div>
+        <span className={`${styles.deleteIcon} material-icons`}>delete</span>
       </div>
     </aside>
   );
